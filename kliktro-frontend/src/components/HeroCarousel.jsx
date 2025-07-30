5b43cf7faa8d75d231692a35e1c908d71f779602
mby444 <bimayudha161@gmail.com>
2025-07-30 14:06:55 +0700

    Changed hero images

--- kliktro-frontend/src/components/HeroCarousel.jsx
+++ kliktro-frontend/src/components/HeroCarousel.jsx
@@ -3,39 +3,26 @@ import { Autoplay, Pagination } from "swiper/modules";
 import "swiper/css";
 import "swiper/css/pagination";
 import { Link } from "react-router";
+import heroBg1 from "@/assets/hero/hero_1.png";
+import heroBg2 from "@/assets/hero/hero_2.png";
+import heroBg3 from "@/assets/hero/hero_3.png";
+import heroMobileBg1 from "@/assets/hero/hero_mobile_1.png";
+import heroMobileBg2 from "@/assets/hero/hero_mobile_2.png";
+import heroMobileBg3 from "@/assets/hero/hero_mobile_3.png";
 
-const images = [
-  "https://picsum.photos/id/1015/1200/600",
-  "https://picsum.photos/id/1020/1200/600",
-  "https://picsum.photos/id/1024/1200/600",
-];
+// const images = [
+//   "https://picsum.photos/id/1015/1200/600",
+//   "https://picsum.photos/id/1020/1200/600",
+//   "https://picsum.photos/id/1024/1200/600",
+// ];
+const images = [heroBg1, heroBg2, heroBg3];
+const imagesMobile = [heroMobileBg1, heroMobileBg2, heroMobileBg3];
 
 export default function HeroCarousel() {
   return (
-    <section className="relative h-[80vh] w-full overflow-hidden">
-      {/* Background Carousel */}
-      <Swiper
-        modules={[Autoplay, Pagination]}
-        spaceBetween={0}
-        slidesPerView={1}
-        loop
-        autoplay={{ delay: 4000 }}
-        pagination={{ clickable: true }}
-        className="absolute inset-0 z-0 h-full">
-        {images.map((src, i) => (
-          <SwiperSlide key={i}>
-            <img
-              src={src}
-              alt={`Slide ${i + 1}`}
-              className="absolute w-full h-full object-cover"
-            />
-            <div className="absolute inset-0 bg-black/40" />
-          </SwiperSlide>
-        ))}
-      </Swiper>
-
+    <section className="relative h-[50rem] w-full overflow-hidden bg-black md:h-[100vh]">
       {/* Overlay Text Content */}
-      <div className="absolute inset-0 z-10 flex flex-col justify-center items-start px-6 md:px-12 text-white max-w-3xl">
+      <div className="relative md:absolute h-1/2 md:h-full inset-0 z-10 flex flex-col justify-center items-start px-6 md:px-12 text-white max-w-3xl">
         <h1 className="text-4xl md:text-5xl font-bold mb-4 drop-shadow-md">
           Discover the Best Electronics
         </h1>
@@ -49,6 +36,33 @@ export default function HeroCarousel() {
           Shop Now
         </Link>
       </div>
+      {/* Background Carousel */}
+      <Swiper
+        modules={[Autoplay, Pagination]}
+        spaceBetween={0}
+        slidesPerView={1}
+        loop
+        autoplay={{ delay: 4000 }}
+        pagination={{ clickable: true }}
+        className="absolute inset-0 z-0 h-1/2 md:h-full">
+        {images.map((src, i) => (
+          <SwiperSlide key={i}>
+            {/* Desktop */}
+            <img
+              src={src}
+              alt={`Slide ${i + 1}`}
+              className="absolute w-full h-full object-cover bg-black hidden md:inline"
+            />
+            {/* Mobile */}
+            <img
+              src={imagesMobile[i]}
+              alt={`Slide ${i + 1}`}
+              className="absolute w-full h-full object-cover bg-black md:hidden"
+            />
+            <div className="absolute inset-0 bg-black/0 md:bg-black/20" />
+          </SwiperSlide>
+        ))}
+      </Swiper>
     </section>
   );
 }
