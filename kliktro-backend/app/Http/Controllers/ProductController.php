d45617803a74cd6558949978f1112b95861e55ed
mby444 <bimayudha161@gmail.com>
2025-07-31 21:31:30 +0700

    Refactor backend

--- kliktro-backend/app/Http/Controllers/ProductController.php
+++ kliktro-backend/app/Http/Controllers/ProductController.php
@@ -24,22 +24,23 @@ class ProductController extends Controller
     {
         return Product::findOrFail($id);
     }
-    
+
     /**
-     * Store a new resource.
+     * Store a newly created resource in storage.
      */
     public function store(Request $request)
     {
-        $image      = $request->file('image');
-        $fileName   = Str::uuid() . '.' . $image->getClientOriginalExtension();
-        $path       = $image->storeAs('images/products', $fileName, "public");
+        $image    = $request->file('image');
+        $fileName = Str::uuid() . '.' . $image->getClientOriginalExtension();
+        $path     = $image->storeAs('images/products', $fileName, 'public');
+
         $product = [
-            "name"=> $request->input("name"),
-            "description"=> $request->input("description"),
-            "price"=> $request->input("price"),
-            "stock"=> $request->input("stock"),
-            "image_url"=> $request->schemeAndHttpHost() . Storage::url($path),
-            "category"=> $request->input("category"),
+            'name'        => $request->input('name'),
+            'description' => $request->input('description'),
+            'price'       => $request->input('price'),
+            'stock'       => $request->input('stock'),
+            'image_url'   => $request->schemeAndHttpHost() . Storage::url($path),
+            'category'    => $request->input('category'),
         ];
 
         return Product::create($product);
@@ -50,24 +51,24 @@ class ProductController extends Controller
      */
     public function update(Request $request, string $id)
     {
-        $product = Product::findOrFail($id);
+        $product    = Product::findOrFail($id);
         $attributes = [
-            "name"=> $request->input("name"),
-            "description"=> $request->input("description"),
-            "price"=> $request->input("price"),
-            "stock"=> $request->input("stock"),
-            "category"=> $request->input("category"),
+            'name'        => $request->input('name'),
+            'description' => $request->input('description'),
+            'price'       => $request->input('price'),
+            'stock'       => $request->input('stock'),
+            'category'    => $request->input('category'),
         ];
 
-        if ($request->hasFile("image")) {
-            $image = $request->file('image');
+        if ($request->hasFile('image')) {
+            $image    = $request->file('image');
             $fileName = Str::uuid() . '.' . $image->getClientOriginalExtension();
-            $path = $image->storeAs('images/products', $fileName, "public");
-    
-            $oldRelativePath = str_replace(asset("storage") . "/", "", $product->image_url);
-    
-            Storage::disk("public")->delete($oldRelativePath);
-            $attributes["image_url"] = $request->schemeAndHttpHost() . Storage::url($path);
+            $path     = $image->storeAs('images/products', $fileName, 'public');
+
+            $oldRelativePath = str_replace(asset('storage') . '/', '', $product->image_url);
+            Storage::disk('public')->delete($oldRelativePath);
+
+            $attributes['image_url'] = $request->schemeAndHttpHost() . Storage::url($path);
         }
 
         $product->update($attributes);
@@ -80,9 +81,10 @@ class ProductController extends Controller
      */
     public function destroy(string $id)
     {
-        $product = Product::findOrFail($id);
-        $relativePath = str_replace(asset('storage') . '/', '', $product->image_url);
+        $product       = Product::findOrFail($id);
+        $relativePath  = str_replace(asset('storage') . '/', '', $product->image_url);
         Storage::disk('public')->delete($relativePath);
+
         $deleted = Product::destroy($id);
 
         return response()->json(['deleted' => $deleted > 0]);
